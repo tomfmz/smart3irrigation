3487486dfd35d70c93e1991f104a7888e89acff1
Tom <luecketom@web.de>
2023-05-11 13:59:26 +0200

    dht22 init commit

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include "DHT.h"                
+#define DHTPIN 2          // Hier die Pin Nummer eintragen wo der Sensor angeschlossen ist
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("DHT11 Testprogramm");
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+                                    
+  float h = dht.readHumidity();    // Lesen der Luftfeuchtigkeit und speichern in die Variable h
+  float t = dht.readTemperature(); // Lesen der Temperatur in °C und speichern in die Variable t
+  
+/*********************( Überprüfen ob alles richtig Ausgelesen wurde )*********************/ 
+  if (isnan(h) || isnan(t)) {       
+    Serial.println("Fehler beim auslesen des Sensors!");
+    return;
+  }
+
+  // Nun senden wir die gemessenen Werte an den PC dise werden wir im Seriellem Monitor sehen
+  Serial.print("Luftfeuchtigkeit: ");
+  Serial.print(h);                  // Ausgeben der Luftfeuchtigkeit
+  Serial.print("%\t");              // Tabulator
+  Serial.print("Temperatur: ");
+  Serial.print(t);                  // Ausgeben der Temperatur
+  Serial.write('°');                // Schreiben des ° Zeichen
+  Serial.println("C");
 }
