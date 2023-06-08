3a2a860382f9d856f3652dae5cdcf1621e849f49
tomfmz <luecketom@web.de>
2023-06-08 21:36:22 +0200

    message conversion

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#define DEBUG 1
 #define SWSERIAL_TX 26
 #define SWSERIAL_RX 27
 #define SWSERIAL_BAUD 9600
 #define HWSERIAL_BAUD 115200 /*!< The baud rate for the output serial port */
 
+void readSMT100(void);
+struct smt100
+{
+   float permittivity;
+   float volwater;
+   float temp;
+   float voltage;
+};
+smt100 smt100_;
+
 EspSoftwareSerial::UART sdiSerial;
 
-String myCommand = "<?M!>";
-String myCommand2 = "<?D0!>";
 
 void setup() {
   Serial.begin(HWSERIAL_BAUD);
@@ -26,10 +35,50 @@ void setup() {
 }
 
 void loop() {
-  sdiSerial.print(myCommand);
+  readSMT100();
+}
+
+void readSMT100(void){
+  String sdiSensorInfo = "<?M!>";
+  String sdiMeasure = "<?D0!>";
+  char c = ' ';
+  const int BUFFER_SIZE = 50;
+  char buf[BUFFER_SIZE];
+  int j = 0;
+  bool marker = false;
+  String stm100_calibrated_permittivity_s;
+  String stm100temp_s;
+  String stm100moisture_s;
+  String stm100voltage_s;
+  sdiSerial.print(sdiSensorInfo);
   delay(300);                    // wait a while for a response
   while (sdiSerial.available()) {  // write the response to the screen
-    Serial.write(sdiSerial.read());
+    c = sdiSerial.read();
   }
-  delay(3000);  // print again in three seconds
+  delay(2000);  // print again in three seconds
+  sdiSerial.print(sdiMeasure);
+  delay(300);                    // wait a while for a response
+  while (sdiSerial.available()) {  // write the response to the screen
+    // read the incoming bytes:
+    int rlen = sdiSerial.readBytes(buf, BUFFER_SIZE);
+    // prints the received data   
+    for (int i = 0; i < rlen; i++) {
+      if (buf[i] == '+') j++;
+      else if (j == 2) stm100_calibrated_permittivity_s += (String)buf[i];
+      else if (j == 3) stm100moisture_s += (String)buf[i];
+      else if (j == 4) stm100temp_s += (String)buf[i];
+      else if (j == 5) stm100voltage_s += (String)buf[i];    
+    }
+  }
+  if (DEBUG){
+    Serial.println("-----SMT100 Data-----");
+    Serial.print("Calibrated permittivity: ");Serial.println(stm100_calibrated_permittivity_s); 
+    Serial.print("Calibrated volumetric water content in percent: ");Serial.println(stm100moisture_s);
+    Serial.print("Temperature: ");Serial.println(stm100temp_s);
+    Serial.print("Voltage: ");Serial.println(stm100voltage_s);
+  }
+  smt100_.permittivity = stm100_calibrated_permittivity_s.toFloat();
+  smt100_.volwater = stm100moisture_s.toFloat();
+  smt100_.temp = stm100temp_s.toFloat();
+  smt100_.voltage = stm100voltage_s.toFloat();
 }
