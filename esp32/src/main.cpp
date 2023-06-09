cd17768ed8646168b68b3cd2690aa864ab356afe
Hauke <hauke.neuber@posteo.de>
2023-06-09 14:34:58 +0200

    init

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <DS1603L.h>
 
 #define DEBUG 1
 #define SWSERIAL_TX 26
 #define SWSERIAL_RX 27
+#define MOSFET_PUMPE 33 
+#define FLOW 39
 #define SWSERIAL_BAUD 9600
 #define HWSERIAL_BAUD 115200 /*!< The baud rate for the output serial port */
 
+void flow_handler(void);
 void readSMT100(void);
+
+const double mls_per_count = 3.0382;
+volatile unsigned long flow_counter = 0;
+unsigned long next_print = 0;
+unsigned long old_counter = 0;
+
 struct smt100
 {
    float permittivity;
@@ -18,10 +28,15 @@ struct smt100
 smt100 smt100_;
 
 EspSoftwareSerial::UART sdiSerial;
+EspSoftwareSerial::UART ds1603LSerial;
 
 
 void setup() {
+  // Hardwareserials
   Serial.begin(HWSERIAL_BAUD);
+  Serial1.begin(9600, SERIAL_8N1, 9, 10); // funktioniert
+  Serial2.begin(9600, SERIAL_8N1, 16, 17); // funktioniert
+
   sdiSerial.begin(SWSERIAL_BAUD, SWSERIAL_8N1, SWSERIAL_RX, SWSERIAL_TX, false);
   if (!sdiSerial) { // If the object did not initialize, then its configuration is invalid
     Serial.println("Invalid EspSoftwareSerial pin configuration, check config"); 
@@ -31,11 +46,32 @@ void setup() {
   }
   while (!Serial)
     ;
+
+  pinMode(MOSFET_PUMPE, OUTPUT);
+  pinMode(FLOW, INPUT);
+  
+  // Die Funktion flowb_handler() als Interrupthandler für steigende Flanken des Durchflusssensors festlegen
+  attachInterrupt(digitalPinToInterrupt(FLOW), flow_handler, RISING);
   delay(500);  // allow things to settle
 }
 
 void loop() {
-  readSMT100();
+  //readSMT100();
+  //digitalWrite(MOSFET_PUMPE, !digitalRead(MOSFET_PUMPE));
+
+  if (millis() >= next_print)
+  {
+    unsigned long current_counter = flow_counter;
+    Serial.println((String) "Flow Counter: " + current_counter);
+    Serial.println((String) "Delta: " + (current_counter - old_counter));
+    old_counter = current_counter;
+    Serial.println((String) (flow_counter * mls_per_count) + " ml");
+    next_print = millis() + 2000;
+  }
+}
+
+void flow_handler() {
+  flow_counter++;
 }
 
 void readSMT100(void){
