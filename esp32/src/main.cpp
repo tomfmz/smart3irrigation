69f10e9f6cccf87455a8ad39ce8ca46f2e3a4bab
tomfmz <luecketom@web.de>
2023-09-06 12:45:09 +0200

    Revert "DHT ohne schleife"
    This reverts commit f59eb00e5c96c076b8472ca1cecd0f1df130184b.

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -226,7 +226,17 @@ void loop() {
 }
 
 // Funktion für das Auslesen des Temperatur-Luftfeuchtigkeitssensors
-void readDHT22(void) { 
+void readDHT22(void) {
+  float humidity = NAN;
+  if (DEBUG)
+    Serial.print("Reading DHT");
+  while (isnan(humidity)) {
+    delay(3000);
+    humidity = dht.readHumidity();
+    if (DEBUG)
+      Serial.print(".");
+  }
+    
   dht22_.humidity = dht.readHumidity();    // Lesen der Luftfeuchtigkeit und speichern in die Variable h
   dht22_.temp = dht.readTemperature(); // Lesen der Temperatur in °C und speichern in die Variable t
   if (DEBUG){
